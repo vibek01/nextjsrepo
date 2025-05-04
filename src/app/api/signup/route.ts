b6a825798711e375ee89de0b7e45179c6ab29ff8
vibek01 <vibekbin305@gmail.com>
2025-05-04 23:44:52 +0530

    singup page.tsx added

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -14,6 +14,7 @@ export async function POST(req: NextRequest){
     } catch (error){
 
         console.error('Signup Error', error);
+        
         return NextResponse.json({success: false, error: 'Server error'}, {status: 500});
     }
 }
