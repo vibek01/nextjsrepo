3e244e607ff125e832f7d29225b6102465235456
vibek01 <vibekbin305@gmail.com>
2025-05-31 13:22:34 +0530

    changed the model of user.ts

--- src/model/user.ts
+++ src/model/user.ts
@@ -7,6 +7,7 @@ export interface User extends Document{
 }
 
 const UserSchema: Schema<User> = new Schema({
+    
     username: {
         type: String,
         required: [true, 'Username is required'],
